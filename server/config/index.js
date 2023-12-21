081b6b4cbd0fea9e0f7dc7f2067c3507c3bda768
2658072675 <66570145+2658072675@users.noreply.github.com>
2023-12-21 19:38:12 +0800

    Update index.js

--- server/config/index.js
+++ server/config/index.js
@@ -7,11 +7,11 @@ function getConfig(key) {
 		port: 3200,  // 端口号，确保没有被占用
 		mysql_config: {  // MySQL配置信息
 			dialect: 'mysql',
-			host: 'xxxx',  // 数据库主机地址
+			host: '43.159.38.166',  // 数据库主机地址
 			port: 3306,  // 数据库端口号，默认3306
-			username: 'chatgptv2',  // 数据库用户名
-			password: 'chatgptv2',  // 数据库密码
-			database: 'chatgptv2',  // 数据库名称
+			username: 'chat130',  // 数据库用户名
+			password: 'ZneCSdQsNbswPcWa',  // 数据库密码
+			database: 'chat130',  // 数据库名称
 			timezone: '+08:00',
 			dialectOptions: {
 				dateStrings: true,
@@ -20,19 +20,19 @@ function getConfig(key) {
 		},
 		redis_config: {  // Redis配置信息，一般不用改
 			type: 'redis',
-			host: 'xxxx',
+			host: '43.159.38.166',
 			port: 6379,
-			password: 'xxxx'
+			password: 'jhkdjhkjdhsIUTYURTU_CzjRAp'
 		},
-		email: 'xxxx',  // 邮件地址
+		email: 'service@sky350.com',  // 邮件地址
 		email_config: {  // 邮件配置信息
-			host: 'smtp.qq.com',  // SMTP服务器地址
+			host: 'smtp.larksuite.com',  // SMTP服务器地址
 			port: 465,  // SMTP服务器端口号
 			ignoreTLS: false,  // 是否忽略TLS
-			secure: true,  // 是否使用安全连接
+			secure: false,  // 是否使用安全连接
 			auth: {
-				user: 'xxxx',  // SMTP服务器用户名
-				pass: 'xxxx'  // SMTP服务器密码
+				user: 'service@sky350.com',  // SMTP服务器用户名
+				pass: 'jGxXarVcHpzbkU7i'  // SMTP服务器密码
 			}
 		}
 	};
